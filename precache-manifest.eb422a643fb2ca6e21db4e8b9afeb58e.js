32a838d5b9df20db969bf5ead40973df808c002b
Kelvin <duchung97dl@gmail.com>
2019-06-28 16:18:02 +0700

    Updates

---
+++ precache-manifest.eb422a643fb2ca6e21db4e8b9afeb58e.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d9eeb629be40ee30b629681a686d9979",
+    "url": "/react_burger/index.html"
+  },
+  {
+    "revision": "9274d255b38888f5ffdb",
+    "url": "/react_burger/static/css/3.904ce67f.chunk.css"
+  },
+  {
+    "revision": "38672455745803bd43cf",
+    "url": "/react_burger/static/css/4.9040c3d0.chunk.css"
+  },
+  {
+    "revision": "71e49d3650133b292ffd",
+    "url": "/react_burger/static/css/5.109aeeb8.chunk.css"
+  },
+  {
+    "revision": "62478e4fc2ded4952214",
+    "url": "/react_burger/static/css/main.16b74c24.chunk.css"
+  },
+  {
+    "revision": "d47a8c1b51c695c623a3",
+    "url": "/react_burger/static/js/2.6dd420d7.chunk.js"
+  },
+  {
+    "revision": "9274d255b38888f5ffdb",
+    "url": "/react_burger/static/js/3.8990d1d8.chunk.js"
+  },
+  {
+    "revision": "38672455745803bd43cf",
+    "url": "/react_burger/static/js/4.9c5b60d4.chunk.js"
+  },
+  {
+    "revision": "71e49d3650133b292ffd",
+    "url": "/react_burger/static/js/5.157c4dd8.chunk.js"
+  },
+  {
+    "revision": "62478e4fc2ded4952214",
+    "url": "/react_burger/static/js/main.29f954bc.chunk.js"
+  },
+  {
+    "revision": "b3557e2f3eaf84bb32d4",
+    "url": "/react_burger/static/js/runtime~main.7fa9bd9d.js"
+  },
+  {
+    "revision": "b8503d262bffbfb7c67fd6762963e7d1",
+    "url": "/react_burger/static/media/burger-logo.b8503d26.png"
+  }
+]);
